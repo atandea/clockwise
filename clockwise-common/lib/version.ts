ae8fd253ce84d833663e1273d225e5f87eebd0c1
Alin Tandea <alin.tandea@protonmail.com>
2026-04-09 16:43:30 +0300

    force github actions to use node 24

---
+++ clockwise-common/lib/version.ts
@@ -0,0 +1,16 @@
+export const versionInfo = {
+  "appVersion": "0.1.0",
+  "nodeEngine": "24",
+  "svelteKit": "2.57.0",
+  "tauriApi": "2.10.1",
+  "buildDate": "Apr 9, 2026, 1:05:32 PM UTC"
+} as const;
+
+export const aboutItems = [
+  { label: "App version", value: versionInfo.appVersion },
+  { label: "Node engine", value: versionInfo.nodeEngine },
+  { label: "SvelteKit", value: versionInfo.svelteKit },
+  { label: "Tauri API", value: versionInfo.tauriApi },
+  { label: "Build date", value: versionInfo.buildDate },
+  { label: "GitHub", value: "https://github.com/atandea/clockwise", href: "https://github.com/atandea/clockwise" },
+] as const;
